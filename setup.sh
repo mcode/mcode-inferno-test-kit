10980631dee3eb5a6cef3312fee784655e403777
Daniel Potter <dpotter@mitre.org>
2024-09-18 16:25:49 -0400

    Remove container on setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker compose pull
 docker compose build
-docker compose run inferno bundle exec inferno migrate
+# --rm tells Docker to remove the container once it completes
+docker compose run --rm inferno bundle exec inferno migrate
